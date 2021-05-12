1697716076cd95bcaec5a24234aea3d100551859
plusone <zhuozhengyi@icloud.com>
2021-05-12 16:10:57 +0800

    Elevator panel inteface done

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
+
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
+    ]
+}
---
+++ src/components/Elevator.vue
@@ -0,0 +1,35 @@
+
+<template>
+<div class="grid grid-cols-5">
+  <ElevatorPanel
+      v-for="ep in this.elevNum"
+      :key="'elev-'+ep"
+      :floor-num="floorNum"
+      :elev-name="'Elev' + ep"
+      :current-floor="1"
+      > </ElevatorPanel>
+</div>
+
+</template>
+
+<script>
+import ElevatorPanel from "@/components/ElevatorPanel";
+
+export default {
+  name: "Elevator",
+  components: {ElevatorPanel},
+  props: {
+  },
+  data() {
+    return {
+      elevNum: 5,
+      floorNum: 20,
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
