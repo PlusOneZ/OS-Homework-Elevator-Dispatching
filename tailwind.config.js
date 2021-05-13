018c898da0777e45a8682aa744d1f3ced4c1b794
plusone <zhuozhengyi@icloud.com>
2021-05-14 00:28:55 +0800

    fix3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: false,
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
