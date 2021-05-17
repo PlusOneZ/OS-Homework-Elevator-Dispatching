5715c4bba98a4e3ed28efc4d47e6c012c5ae1092
plusone <zhuozhengyi@icloud.com>
2021-05-17 14:46:01 +0800

    new page bugged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './src/**/*.html',
+        './src/**/*.js',
+        './src/**/*.vue'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
